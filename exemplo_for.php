593b0fd2e496a172562d8f388cf2eb2f63c99edb
LETICIA FERNANDA PEREIRA <leticia.6978@aluno.pr.senac.br>
2023-04-03 08:15:02 -0300

    for

---
+++ exemplo_for.php
@@ -0,0 +1,32 @@
+<?php
+    //Laço de Repetição FOR em php
+
+    //Imprima os valores de 1 a 10 em sua página
+
+    echo "1";
+    echo"</br>";
+    echo "2";
+    echo"</br>";
+    echo "3";
+    echo"</br>";
+    echo "4";
+    echo"</br>";
+    echo "5";
+    echo"</br>";
+    echo "6";
+    echo"</br>";
+    echo "7";
+    echo"</br>";
+    echo "8";
+    echo"</br>";
+    echo "9";
+    echo"</br>";
+    echo "10";
+    echo"</br>";
+
+
+    for($numero=0; $numero<=10; $numero++){
+        echo $numero . "</br>";
+    }
+
+?>
---
+++ for_numerosimpar.php
@@ -0,0 +1,8 @@
+<?php
+    for($numero=0; $numero<=100; $numero++){
+       if($numero % 2 == 1){
+        echo $numero . "</br>";
+       }
+
+    }
+?>
